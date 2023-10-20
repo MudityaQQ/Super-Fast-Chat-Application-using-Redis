88c302274d948be84e38f05af16b2d92b2e50abf
MudityaQQ <147902558+MudityaQQ@users.noreply.github.com>
2023-10-20 10:14:24 +0530

    Added Upstash DB

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Button from './components/ui/Button'
+import { db } from './lib/db'
 
-export default function Home() {
+export default async function  Home() {
+
+  await db.set('hello', 'jello');
   return (
   <main>
     <h1 className='text-red-900'>hello world</h1>
